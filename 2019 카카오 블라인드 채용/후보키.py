65ce512fdbb21523e7681603a9dd9c2eb33b8899
DojinPark <74354737+DojinPark@users.noreply.github.com>
2021-02-03 16:06:01 +0900

    Create 후보키.py

---
+++ 2019 카카오 블라인드 채용/후보키.py
@@ -0,0 +1,49 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42890
+# 후보키
+# 걸린시간: 0:33
+#
+# combination과 set을 이용하는 문제
+
+from itertools import combinations
+from copy import deepcopy
+
+def solution(relation):
+    answer = 0
+    
+    rows = len(relation)
+    cols = len(relation[0])
+
+    keys = []
+    for length in range(1, cols + 1):
+        keys += combinations(range(cols), length)
+
+    candidates = set()
+
+    # 작은 길이의 키 조합부터 확인
+    for key in keys:
+
+        # 데이터의 각 행별
+        # key에 해당하는 열을 set 자료형에 모아서
+        # set의 크기가 전체 행의 수와 같으면 uniqueness 만족
+        uniqueness = set()
+        for row in relation:
+            uniqueness.add(tuple([row[i] for i in key]))
+        if len(uniqueness) != rows: continue
+
+        # 이전에 저장한 candidate key 중 
+        # 현재 key의 부분 집합이 있으면 minimality를 만족하지 않음
+        not_candidate = False
+        for candidate in candidates:
+            if set(candidate) & set(key) == set(candidate):
+                not_candidate = True
+                break
+            
+        if not_candidate: continue
+        candidates.add(key)
+    
+    answer = len(candidates)
+
+    return answer
+
+relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
+print( solution(relation) ) # 2
