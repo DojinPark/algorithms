5a81bdbeb40026b790fcaf0a1075da3b884faed0
DojinPark <74354737+DojinPark@users.noreply.github.com>
2021-02-22 17:06:11 +0900

    Update 그래프 알고리즘 2 (MST).py

--- 코드 연습장/그래프 알고리즘 2 (MST).py
+++ 코드 연습장/그래프 알고리즘 2 (MST).py
@@ -32,6 +32,10 @@ def union(roots, i, j):
 def is_cycle(roots, i, j):
     return get_root(roots, i) == get_root(roots, j)
 
+# 크루스칼 알고리즘의 코드 바디 자체는 간단하다.
+# 힙에다 간선을 싹 다 넣은 뒤
+# 다시 하나 씩 힙에서 빼면서
+# 사이클이 형성 되지 않는 간선이면 추가한다.
 from heapq import heappush as push, heappop as pop
 def kruskal(V, edges_str):
     edges = [ map(int, e.split()) for e in edges_str ]
@@ -39,6 +43,7 @@ def kruskal(V, edges_str):
     roots = [ i for i in range(V + 1) ]
     h = []
     sum = 0
+    cnt = 0 # Early-Stop
 
     for a, b, c in edges:
         push(h, (c, a, b))
@@ -47,6 +52,8 @@ def kruskal(V, edges_str):
         c, a, b = pop(h)
         if not is_cycle(roots, a, b):
             sum += c
+            cnt += 1                # Early-
+            if cnt == V - 1: break  # -Stop
             union(roots, a, b)
 
     return sum
