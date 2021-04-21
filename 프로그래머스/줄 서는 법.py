aa917a63c4d163093b7f12839be3b69a2283c382
DojinPark <74354737+DojinPark@users.noreply.github.com>
2021-04-21 16:23:28 +0900

    Update 줄 서는 법.py

--- 프로그래머스/줄 서는 법.py
+++ 프로그래머스/줄 서는 법.py
@@ -6,22 +6,23 @@
 
 def solution(n, k):
     answer = []
-    nums = list( range(1, n+1) )
+    nums = list( range(1, n+1) ) # nums: 순열에 들어갈 자연수 리스트
 
-    k -= 1
+    k -= 1 # 인덱스 계산을 위해 k에서 1을 빼고 시작
 
     div = 1
     for f in range(1, n+1):
         div *= f
     
     while nums:
-        div //= n
-        i = k // div
-        k %= div
-        n -= 1
+        div //= n # div: 오른쪽에서 n번째 수가 같은 경우의 수
+        i = k // div # nums에서 남은 수
         
         answer.append(nums[i])
         del nums[i]
+        
+        k %= div
+        n -= 1
 
     return answer
 
